215ea4dd686efcb00ee7800164c96c2d7533dffd
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2023-01-07 08:03:16 +0100

    idk

--- DSP_diarization_demo.py
+++ DSP_diarization_demo.py
@@ -12,8 +12,9 @@ speaker_names = ["Akaash", "Sander"]
 speaker_wavs = [wav[int(s[0] * sampling_rate):int(s[1] * sampling_rate)] for s in segments]
 
 encoder = VoiceEncoder("cpu")
-print("Running the continuous embedding on cpu, this might take a while...")
+print("Running the continuous embedding on cpu, this might take a whilep...")
 _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=2)
+
 
 speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
 similarity_dict = {name: cont_embeds @ speaker_embed for name, speaker_embed in 
