f891c28ee2684973bcb7ee02a9c4b02626383ab9
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2023-01-10 01:56:27 +0100

    Added new audio sample and modified demo

--- DSP_diarization_demo.py
+++ DSP_diarization_demo.py
@@ -2,18 +2,18 @@ from resemblyzer import preprocess_wav, VoiceEncoder
 from demo_utils import *
 from pathlib import Path
 
-wav_fpath = Path("audio_data", "dirt_mouth.mp3")
+wav_fpath = Path("audio_data", "spaceballs.mp3")
 wav = preprocess_wav(wav_fpath)
 
-segments = [[8, 12], [30, 33]]
-speaker_names = ["Akaash", "Sander"]
+segments = [[5, 11], [91, 96], [98, 100]]
+speaker_names = ["Akaash", "Sander", "Wesley"]
 
 # extract wave segments from wave based on the specified segments above
 speaker_wavs = [wav[int(s[0] * sampling_rate):int(s[1] * sampling_rate)] for s in segments]
 
 encoder = VoiceEncoder("cpu")
 print("Running the continuous embedding on cpu, this might take a whilep...")
-_, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=2)
+_, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=32)
 
 
 speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
